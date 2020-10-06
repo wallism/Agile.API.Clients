471456e1f5c16252ffccd9ad6c96018d589cd60c
wallism <mark.wallis@rockend.com.au>
2020-10-06 14:20:42 +1100

    include FormUrlEncoded

--- Agile.API.Clients/Helpers/MediaTypes.cs
+++ Agile.API.Clients/Helpers/MediaTypes.cs
@@ -2,6 +2,9 @@
 
 namespace Agile.API.Clients.Helpers
 {
+    /// <summary>
+    /// Content Type
+    /// </summary>
     public static class MediaTypes
     {
         public static MediaTypeHeaderValue JSON = new MediaTypeHeaderValue("application/json");
@@ -9,5 +12,6 @@ namespace Agile.API.Clients.Helpers
         public static MediaTypeHeaderValue XML = new MediaTypeHeaderValue("application/xml");
         public static MediaTypeHeaderValue HTML = new MediaTypeHeaderValue("text/html");
         public static MediaTypeHeaderValue JPEG = new MediaTypeHeaderValue("image/jpeg");
+        public static MediaTypeHeaderValue FormUrlEncoded = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
     }
 }
